c0d110ea9389ce966ccac7203d1c9f7bb253d59c
Shebin <shebin@interlandtech.com>
2026-02-25 15:31:39 +0530

    initial project structure with all service modules

---
+++ restuarant-service/src/main/java/com/quickbite/restuarantservice/RestuarantServiceApplication.java
@@ -0,0 +1,13 @@
+package com.quickbite.restuarantservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestuarantServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestuarantServiceApplication.class, args);
+    }
+
+}
---
+++ restuarant-service/src/test/java/com/quickbite/restuarantservice/RestuarantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickbite.restuarantservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestuarantServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
