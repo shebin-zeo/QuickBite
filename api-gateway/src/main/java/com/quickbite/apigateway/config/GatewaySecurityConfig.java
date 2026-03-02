caa34a9fb66237e34a7605d2b2faf6d2cb15c2c2
Shebin <shebin@interlandtech.com>
2026-03-02 16:37:02 +0530

    QB-004: Update Gateway security configuration

---
+++ api-gateway/src/main/java/com/quickbite/apigateway/config/GatewaySecurityConfig.java
@@ -0,0 +1,28 @@
+package com.quickbite.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class GatewaySecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/actuator/**").permitAll()
+                        .pathMatchers("/public/**").permitAll()
+
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+
+        return http.build();
+    }
+}
